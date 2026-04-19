ca955c0d0338858d72b7e12382eb7f4fab4a7cbf
DeLoR1AnEc <cheatgameminecraft@gmail.com>
2026-04-19 18:11:49 +0200

    changes

--- modules/aspects/terminal.nix
+++ modules/aspects/terminal.nix
@@ -8,19 +8,17 @@
     maid =
       { pkgs, ... }:
       {
-        file.xdg_config."foot/foot.ini" = {
-          text = ''
-            [main]
-            term=foot
-            font=Maple Mono NF:size=13
-            dpi-aware=no
-            resize-keep-grid=no
-            shell=${pkgs.bash}/bin/bash --login -c 'nu --login --interactive'
+        file.xdg_config."foot/foot.ini".text = ''
+          [main]
+          term=foot
+          font=Maple Mono NF:size=13
+          dpi-aware=no
+          resize-keep-grid=no
+          shell=${pkgs.bash}/bin/bash --login -c 'nu --login --interactive'
 
-            [mouse]
-            hide-when-typing=yes
-          '';
-        };
+          [mouse]
+          hide-when-typing=yes
+        '';
       };
   };
 }
