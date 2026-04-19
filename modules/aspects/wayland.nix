d21aee89378f469b31d07e95c587f0094a7bb53b
DeLoR1AnEc <cheatgameminecraft@gmail.com>
2026-04-19 17:07:28 +0200

    changes

--- modules/aspects/wayland.nix
+++ modules/aspects/wayland.nix
@@ -12,14 +12,14 @@
 
     maid =
       {
-        file.".config/gtk-3.0/bookmarks".text = ''
+        file.xdg_config."gtk-3.0/bookmarks".text = ''
           "file:///{{home}}/downloads Downloads"
           "file:///{{home}}/documents Documents"
           "file:///{{home}}/coding Coding"
           "file:///{{home}}/pictures Pictures"
         '';
 
-        file.".config/gtk-3.0/settings.ini".text = ''
+        file.xdg_config."gtk-3.0/settings.ini".text = ''
           [Settings]
           gtk-decoration-layout=appmenu:none
         '';
