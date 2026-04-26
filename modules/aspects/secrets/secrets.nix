228f1f9f7a1659237d0713839a7e604f9b106eee
DeLoR1AnEc <cheatgameminecraft@gmail.com>
2026-04-26 17:36:19 +0200

    changes

--- modules/aspects/secrets/secrets.nix
+++ modules/aspects/secrets/secrets.nix
@@ -3,25 +3,25 @@ let
   sopsConfig = {
     defaultSopsFile = ./secrets.yaml;
     defaultSopsFormat = "yaml";
-    age.keyFile = "/var/lib/sops-age-key";
+    age.keyFile = "/var/lib/secrets/sops/age/keys.txt";
   };
 in
 {
   den.aspects.secrets = {
     nixos =
+      { pkgs, ... }:
       {
         imports = [ inputs.sops-nix.nixosModules.default ];
+        environment.systemPackages = with pkgs; [
+          age sops
+        ];
         sops = sopsConfig;
       };
 
-    maid =
-      { pkgs, ... }:
-      {
-        packages = [ pkgs.sops ];
-
-        file.home.".bash_profile".text = ''
-          export SOPS_AGE_KEY_FILE="${sopsConfig.age.keyFile}"
-        '';
-      };
+    maid = {
+      file.home.".bash_profile".text = ''
+        export SOPS_AGE_KEY_FILE="${sopsConfig.age.keyFile}"
+      '';
+    };
   };
 }
