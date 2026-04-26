38a9a59b564b33741e489da18d5fcfafb0396ee0
DeLoR1AnEc <cheatgameminecraft@gmail.com>
2026-04-26 11:33:01 +0200

    changes

--- modules/aspects/presets.nix
+++ modules/aspects/presets.nix
@@ -11,20 +11,27 @@
     <git>
     <apps>
     <shell>
+
+    <boot/secure>
+    <boot/greeter>
+    <preservation>
+    <zram>
+
+    <terminal>
+    <browser>
+    <niri>
   ];
 
   den.aspects.presets._ = {
-    desktop = {
-      includes = [
-        <boot/secure>
-        <boot/greeter>
-        <preservation>
-        <zram>
+    desktop.includes = [
+      # <boot/secure>
+      # <boot/greeter>
+      # <preservation>
+      # <zram>
 
-        <terminal>
-        <browser>
-        <niri>
-      ];
-    };
+      # <terminal>
+      # <browser>
+      # <niri>
+    ];
   };
 }
